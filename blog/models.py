0835fb6f54199a285145c44b15bf3a40de3dfb84
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-03-28 17:57:22 +0900

    도전과제

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/',blank=True)
     file_upload = models.FileField(upload_to='blog/filems/%Y/%m/%d/',blank=True)
+    hook_message = models.TextField(blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)

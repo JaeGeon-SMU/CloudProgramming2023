b0b0de1117ebd02dc58025bf524c1b7aa6750540
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-03-28 17:24:50 +0900

    첨부파일 다운로드 추가, 이미지 없는 글 오류 해결

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
+import os.path
+
 from django.db import models
 
 #요거 건들면 makemigrations 해야함
@@ -18,3 +20,6 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)

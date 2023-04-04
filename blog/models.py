bd3fbb0f91c5d40785eb57d83acc3c52fc1b0eb3
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-04-04 16:16:06 +0900

    모델에 카테고리 추가

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,17 @@ import os.path
 from django.contrib.auth.models import User
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=20,unique=True)
+    #slug 읽을수 있는 텍스트로 된 url사용하고 싶을때 사용
+    slug = models.SlugField(max_length=50, unique=True , allow_unicode=True)
+
+    def __str__(self):
+     return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 #요거 건들면 makemigrations 해야함
 
 # Create your models here.
@@ -17,7 +28,8 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    author=models.ForeignKey(User,null=True,on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category,null=True,on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'[{self.pk}] {self.title} - {self.author}'

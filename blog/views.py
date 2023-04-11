f14f006428e39660fa37c0cdfe6a876e2e61e932
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-04-11 17:33:08 +0900

    d

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,61 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from .models import Post, Category, Tag
 
 
+class PostUpdate(LoginRequiredMixin,UpdateView):
+    model=Post
+    fields = ['title', 'content', 'head_image', 'file_upload', 'category', 'tag']
+
+    template_name = 'blog/post_update.html'
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and self.get_object().author == request.user:
+            return super(PostUpdate,self).dispatch(request,*args,**kwargs)
+        else:
+            return PermissionError
+class PostCreate(LoginRequiredMixin,UserPassesTestMixin,CreateView):
+    model = Post
+    fields = ['title', 'content', 'head_image', 'file_upload', 'category', 'tag']
+
+
+    def test_func(self):
+        return self.request.user.is_staff or self.request.user.is_superuser
+
+    #등록되고 staff 나 superuser여야함
+    def form_valid(self, form):
+        if self.request.user.is_authenticated and (self.request.user.is_superuser or self.request.user.is_staff):
+            form.instance.author = self.request.user
+            return super(PostCreate,self).form_valid(form)
+        else:
+            return redirect('/blog/')
+    def get_context_data(self, **kwargs):
+        context=super(PostCreate,self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_count'] = Post.objects.filter(category=None).count()
+
+        return context
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+
+
 
     def get_context_data(self, **kwargs):
         context=super(PostList,self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_count'] = Post.objects.filter(category=None).count()
-
         return context
 
 class PostDetail(DetailView):
     model = Post
+    def get_context_data(self, **kwargs):
+        context=super(PostDetail,self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_count'] = Post.objects.filter(category=None).count()
+
+        return context
 
 
 def categories_page(request,slug):
@@ -30,7 +69,8 @@ def categories_page(request,slug):
             'category' : category,
             'categories' : Category.objects.all(),
             'post_list' : post_list,
-            'no_category_count' : Post.objects.filter(category=None).count()
+            'no_category_count' : Post.objects.filter(category=None).count(),
+
     }
     return render(request,'blog/post_list.html',context)
 

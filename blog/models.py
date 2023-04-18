0ab1ff2d7291418edd7d97dab94023d735f28708
JaeGeon-SMU <81796331+JaeGeon-SMU@users.noreply.github.com>
2023-04-18 15:44:39 +0900

    마크다운, 폼 추가

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,9 @@ import os.path
 
 from django.contrib.auth.models import User
 from django.db import models
+from markdown import markdown
+from markdownx.models import MarkdownxField
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=20, unique=True)
@@ -32,7 +35,7 @@ class Category(models.Model):
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=30)
-    content = models.TextField()
+    content = MarkdownxField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/',blank=True)
     file_upload = models.FileField(upload_to='blog/filems/%Y/%m/%d/',blank=True)
@@ -53,3 +56,6 @@ class Post(models.Model):
 
     def get_file_name(self):
         return os.path.basename(self.file_upload.name)
+
+    def get_content_markdown(self):
+        return markdown(self.content)
